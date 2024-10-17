16c2c4a162132f229d638b066e6901b56067c25b
nvanzeijl <nvanzeijl@gmail.com>
2024-10-17 12:15:40 +0200

    opdracht 2A en 2B gedaan, alleen 2C en bonus

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -16,22 +16,22 @@
 
 // ---- Verwachte uitkomst: 6
 
-//Stappenplan:
+//Stappenplan 1A:
 // 1.Maak een opslag plaats waar we opslaan dat er een 8 of hoger is gevonden. alle keren dat er een 8 is gevonden
 // 2. Loop door de array en check elke waarde op 8 of hoger.
 // a) is 8 of hoger sla dan op in de gecreeerde opslagplaats.
 // b) is minder dan 8 dan gewoon verder gaan met checken array.
 // 3. geef het aantal keren dat er 8 of hoger is gescored, terug.
 
-const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
+//const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 
-let cumLaudeCount2 = 0;
+//let cumLaudeCount2 = 0;
 
-for (let i = 0; i < grades.length; i++) {
-    cumLaudeCount2 += grades[i] >= 8;
-}
+//for (let i = 0; i < grades.length; i++) {
+//    cumLaudeCount2 += grades[i] >= 8;
+//}
 
-console.log(cumLaudeCount2);
+//console.log(cumLaudeCount2);
 
 
 /*  1b: Omschrijven tot een herbruikbare functie   */
@@ -44,21 +44,21 @@ console.log(cumLaudeCount2);
 // cumLaude([6, 4, 5]) geeft 0
 // cumLaude([8, 9, 4, 6, 10]) geeft 3
 
-const grades1 = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
-const grades2 = [6, 4, 5];
-const grades3 = [8, 9, 4, 6, 10];
+//const grades1 = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
+//const grades2 = [6, 4, 5];
+//const grades3 = [8, 9, 4, 6, 10];
 
-function cumLaude(grade) {
-    let cumLaudeCount = 0;
-  for (let i = 0; i < grade.length; i++) {
-      if (grade[i] >= 8) {
-         cumLaudeCount++;
-      }
-  } return cumLaudeCount;
-}
+//function cumLaude(grade) {
+//    let cumLaudeCount = 0;
+//  for (let i = 0; i < grade.length; i++) {
+//      if (grade[i] >= 8) {
+//         cumLaudeCount++;
+//      }
+//  } return cumLaudeCount;
+//}
 
-const totalCumLaude = cumLaude(grades3);
-console.log(totalCumLaude);
+//const totalCumLaude = cumLaude(grades3);
+//console.log(totalCumLaude);
 
 
 
@@ -74,6 +74,24 @@ console.log(totalCumLaude);
 
 // ---- Verwachte uitkomst: 6.642857142857143
 
+//Stappenplan:
+// 1. Loop de array door.
+// 2. Tel telkens de nieuwe waarde bij de oude waarde op.
+// 3. Deel het totaal van de optelsom door het aantal items van de array
+// 4. Geef het resultaat terug.
+
+const grades4 = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
+const grades5 = [6, 4, 5];
+const grades6 = [8, 9, 4, 6, 10];
+
+let counttotal = 0;
+
+for (let i = 0; i < grades4.length; i++) {
+    counttotal += grades4[i];
+}
+const averageGrade1 = counttotal / grades4.length;
+console.log(averageGrade1);
+
 
 /* 2b: Omschrijven tot een herbruikbare functie */
 // Schrijf een functie genaamd averageGrade, die een array van cijfers verwacht (zoals grades) en het gemiddelde cijfer teruggeeft. Gebruik hiervoor jouw antwoord van 2a.
@@ -84,6 +102,17 @@ console.log(totalCumLaude);
 // averageGrade(grades) geeft 6.642857142857143
 // averageGrade([6, 4, 5]) geeft 5
 // averageGrade([8, 9, 4, 6, 10]) geeft 7.4
+
+function averageGrade (grades) {
+    let totalCount = 0;
+    for (let i = 0; i < grades.length; i++) {
+        totalCount += grades[i];
+    }
+    return totalCount / grades.length;
+}
+
+const gradeAverage = averageGrade(grades6);
+console.log(gradeAverage);
 
 
 /* 2c: Afronden op twee decimalen */
